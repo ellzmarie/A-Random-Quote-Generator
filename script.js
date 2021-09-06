abad8c06ab7cba4a0c0aab93f2394330772a5eba
Ella <76459774+ellzmarie@users.noreply.github.com>
2021-09-06 07:10:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,48 +12,48 @@ project 1 - A Random Quote Generator
 ***/
 var quotes = [{
     quote: `Everything in the universe has a rhythm, everything dances.`,
-    source: `Maya Angelou`,
-    citation: `Pocket Maya Angelou Wisdom`,
+    source: `Maya Angelou `,
+    citation: `Pocket Maya Angelou Wisdom `,
     year: 2019
   },
   {
     quote: `The truth is, no one of us can be free until everybody is free.`,
-    source: `Maya Angelou`,
-    citation: `Pocket Maya Angelou Wisdom`,
+    source: `Maya Angelou `,
+    citation: `Pocket Maya Angelou Wisdom `,
     year: 2019
   },
   {
-    quote: `I think a hero is any person really intent on making this a better place for all people.`,
-    source: `Maya Angelou`,
+    quote: `I think a hero is any person really intent on making this a better place for all people. `,
+    source: `Maya Angelou `,
     year: 2019
   },
   {
-    quote: `If you are always trying to be normal, you will never know how amazing you can be.`,
-    source: `Maya Angelou`,
+    quote: `If you are always trying to be normal, you will never know how amazing you can be. `,
+    source: `Maya Angelou `,
     citation: `Pocket Maya Angelou Wisdom`,
   },
   {
-    quote: `Do the best you can until you know better. Then when you now better, do better.`,
-    source: `Maya Angelou`,
-    citation: `Pocket Maya Angelou Wisdom`,
+    quote: `Do the best you can until you know better. Then when you now better, do better. `,
+    source: `Maya Angelou `,
+    citation: `Pocket Maya Angelou Wisdom `,
     year: 2019
   },
   {
-    quote: `You may not control all the events that happen to you, but you can decide not to be reduced by them.`,
-    source: `Maya Angelou`,
-    citation: `Pocket Maya Angelou Wisdom`,
+    quote: `You may not control all the events that happen to you, but you can decide not to be reduced by them. `,
+    source: `Maya Angelou `,
+    citation: `Pocket Maya Angelou Wisdom `,
     year: 2019
   },
   {
-    quote: `I have learnt that people will forget what you said, people will forget what you did, but peole will never forget how you made them feel.`,
-    source: `Maya Angelou`,
-    citation: `Pocket Maya Angelou Wisdom`,
+    quote: `I have learnt that people will forget what you said, people will forget what you did, but peole will never forget how you made them feel. `,
+    source: `Maya Angelou `,
+    citation: `Pocket Maya Angelou Wisdom `,
     year: 2019
   },
   {
-    quote: `Be a rainbow in somebody else’s cloud.`,
-    source: `Maya Angelou`,
-    citation: `https://www.huffpost.com/entry/maya-angelou-oprah-rainbow_n_5413544`,
+    quote: `Be a rainbow in somebody else’s cloud. `,
+    source: `Maya Angelou `,
+    citation: `https://www.huffpost.com/entry/maya-angelou-oprah-rainbow_n_5413544 `,
     year: 2014
   }];
 
